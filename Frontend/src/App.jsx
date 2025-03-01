4bb8b8442a224d324a76efb74b736caecef3ffbf
Siddhesh Todi <siddheshtodi@gmail.com>
2025-03-01 12:21:34 +0530

    Updated frontend and completed making Home Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import UserLogin from './pages/UserLogin';
+import UserSignUp from './pages/UserSignUp';
+import CaptainLogin from './pages/CaptainLogin';
+import CaptainSignUp from './pages/CaptainSignUp';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<UserLogin />} />
+                <Route path='/signup' element={<UserSignUp />} />
+                <Route path='/captain-login' element={<CaptainLogin />} />
+                <Route path='/captain-signup' element={<CaptainSignUp />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App
+export default App;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <div>
+            <div className='bg-cover bg-right bg-[url(https://images.unsplash.com/photo-1613275763543-77beef92a3d0?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen w-full pt-8 bg-red-400 flex justify-between flex-col'>
+                <img className='w-15 ml-9' src='https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png' />
+                <div className='bg-white pb-7 py-4 px-4'>
+                    <h2 className='text-3xl font-bold'>Get started with Uber</h2>
+                    <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
